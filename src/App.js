72159969af0b5f58da58dd5c19512382ef3cf9e5
Angel Valdivia <angelv1901@gmail.com>
2019-08-09 17:54:34 -0400

    Add helper functions to count characters.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import PeopleList from "./components/PeopleList";
 import axios from "axios";
+import CharacterCountList from "../src/components/CharacterCountList";
 
 function App() {
   const [people, setPeople] = useState([]);// initailize people using hooks 'useState'
@@ -14,25 +15,52 @@ function App() {
 		
   };
   
-  const arrayOfEmails = listOfPeople => {
-    return listOfPeople.map(person => {
-      return person.email_address;
-    });
-  };
+  // subscribe and unsubscribe from fetchUsers
+ useEffect(() => {
+   fetchPeople(people);
+   setEmails(ListAllEmails(people));
+ }, [people]);
 
-  
-  
+ // function to capture emails from every person from list of people and return array
+ const ListAllEmails = arrayOfPeople => {
+   return arrayOfPeople.map(singleEmail => {
+     return singleEmail.email_address;
+   });
+ };
 
-	useEffect(() => {
-    fetchPeople(people);
-    setEmails(arrayOfEmails(people))
-    console.log(emails)
-	}, [people]);
-	return (
-		<div className="App">
-			<PeopleList list={people} />
-		</div>
-	);
+ // function to count each character and return an object
+ const characterCount = emailArr => {
+   let repeatingCharacters = emailArr.join("").split("");
+
+   let characterCount = {};
+
+   repeatingCharacters.forEach(char => {
+     if (characterCount[char]) {
+       // if character exists again increase count
+       characterCount[char]++;
+     } else {
+       // else equal count to 1
+       characterCount[char] = 1;
+     }
+   });
+   return characterCount;
+ };
+
+ const characterList = characterCount(emails);
+
+ return (
+   <div
+     style={{
+       display: "flex",
+       flexDirection: "row",
+       justifyContent: "space-around"
+     }}
+   >
+     <PeopleList list={people} />
+     <CharacterCountList charactersList={characterList} />
+   </div>
+ );
 }
 
 export default App;
+
